4063c776375ae279bb7179d27aa2dd390debeee2
Daniel Budiharto <141699797+slidingsoul@users.noreply.github.com>
2024-06-20 16:02:49 +0700

    Revert "mengganti destinasi db dari tanmuldb ke tanmuldb jadi"
    This reverts commit a83944ab8d11c9eb67a40d79a08c8f1a20792010.

--- tanmul API/models/setup.go
+++ tanmul API/models/setup.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/tanmuldbjadi"))
+	database, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/tanmuldb"))
 	if err != nil {
 		panic(err)
 	}
