b9976e97b0d57936ad85cfcab6ec0619d98660d3
Daniel Budiharto <141699797+slidingsoul@users.noreply.github.com>
2024-06-20 15:56:12 +0700

    Revert "update angka input tidak boleh negatif"
    This reverts commit 46dd9692bbfc2d43b2bc761c54b0c5435b03d044.

--- tanmul API/models/trans_detail.go
+++ tanmul API/models/trans_detail.go
@@ -6,7 +6,7 @@ type Trans_detail struct {
 	Nama_bar     string `gorm:"type:varchar(50)" json:"nama_bar" binding:"required"`
 	Jml_pcs_jual int64  `gorm:"int" json:"jml_pcs_jual" binding:"required"`
 	Hrg_jual     int64  `gorm:"int" json:"hrg_jual" binding:"required"`
-	Diskon       int64  `gorm:"int" json:"diskon"`
+	Diskon       int64  `gorm:"int" json:"diskon" binding:"required"`
 }
 
 func (Trans_detail) TableName() string {
